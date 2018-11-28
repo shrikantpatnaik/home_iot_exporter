9ff7ad3239ee4ba67c845d2230cac70470cd56d2
Shrikant Patnaik <shrikant.patnaik@gmail.com>
2018-11-28 17:25:28 +0800

    Use Gorilla Mux as router and allow graceful shutdown

--- main.go
+++ main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"context"
+	"flag"
+	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
 	"time"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -21,7 +27,7 @@ var (
 	iotMetric = prometheus.NewGaugeVec(prometheus.GaugeOpts{
 		Namespace: "sp_home",
 		Subsystem: "iot",
-		Name:      "homeiot_metric",
+		Name:      "metric",
 		Help:      "Metric for Home IOT Network",
 	},
 		[]string{
@@ -35,7 +41,48 @@ func init() {
 }
 
 func main() {
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
 	recordMetrics()
-	http.Handle("/metrics", promhttp.Handler())
-	http.ListenAndServe(":2112", nil)
+	r := mux.NewRouter()
+	r.Handle("/metrics", promhttp.Handler()).Methods("GET")
+
+	srv := &http.Server{
+		Addr:         "0.0.0.0:8080",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r, // Pass our instance of gorilla/mux in.
+	}
+
+	// Run our server in a goroutine so that it doesn't block.
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	if err := srv.Shutdown(ctx); err != nil && err != context.Canceled {
+		log.Println(err)
+	}
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
+
 }
